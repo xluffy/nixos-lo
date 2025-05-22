3aede3dd9e44748e53468c32901117e9d8eb3ed3
xluffy <quang@2meo.com>
2025-05-22 22:01:57 +0700

    Init new instance for testing

---
+++ infra/nixos.tf
@@ -0,0 +1,9 @@
+module "nixos_tinker" {
+  source = "./modules/instance"
+
+  label           = "gh-nixos-tinker-001"
+  image           = "linode/ubuntu22.04"
+  type            = "g6-nanode-1"
+  tags            = ["linode", "nixps", "infra", "gh-nixos-tinker-001"]
+  authorized_keys = [linode_sshkey.xluffy.ssh_key]
+}
